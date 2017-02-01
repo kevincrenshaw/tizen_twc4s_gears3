998c2ca041b3aa7b6f11f2221c3de159592d994f
Maciej Malak <maciej.malak@intive.com>
2017-02-01 17:31:00 +0100

    App versioning scheme update

--- js/about.js
+++ js/about.js
@@ -1,6 +1,6 @@
 /* jshint esversion: 6 */
 ( function () {
-	const currentReleaseVersion = "1.0.";
+	const currentReleaseVersion = "0.0.1.";
 	const jenkinsBuildNumber = "37";
 	const versionInfo = TIZEN_L10N.ABOUT_APP_VERSION.concat("<br>", currentReleaseVersion, jenkinsBuildNumber);
 	const versionLabel = document.getElementById('version');
