0c8d96c91134d4ef4c307f41a86d29c77a838407
Maciej Malak <maciej.malak@intive.com>
2017-02-01 16:54:59 +0100

    Error messages when views were not found

--- js/about.js
+++ js/about.js
@@ -4,5 +4,10 @@
 	const jenkinsBuildNumber = "37";
 	const versionInfo = TIZEN_L10N.ABOUT_APP_VERSION.concat("<br>", currentReleaseVersion, jenkinsBuildNumber);
 	const versionLabel = document.getElementById('version');
+	
+	if (!versionLabel) {
+		console.error("about.js :: #version <p> from about.html not found in hierarchy");
+	}
+	
 	versionLabel.innerHTML = versionInfo;
 } () );
