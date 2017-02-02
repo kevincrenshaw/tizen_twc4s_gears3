0e173a99501a6f6dc91c072b4aaca955d4de8d0d
Maciej Malak <maciej.malak@intive.com>
2017-02-02 16:47:14 +0100

    View id name change

--- js/mainmenu/settings/information.js
+++ js/mainmenu/settings/information.js
@@ -3,7 +3,7 @@
 	const currentReleaseVersion = "0.0.1.";
 	const jenkinsBuildNumber = "137.5c833e7";
 	const versionInfo = TIZEN_L10N.ABOUT_APP_VERSION.concat("<br>", currentReleaseVersion, jenkinsBuildNumber);
-	const versionLabel = document.getElementById('version');
+	const versionLabel = document.getElementById('app-version');
 	
 	if (!versionLabel) {
 		console.error("about.js :: #version <p> from about.html not found in hierarchy");
