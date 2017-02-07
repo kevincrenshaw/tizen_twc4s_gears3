e6260eb7b4c642c6791245b595ce7f3f8005b6e4
Mariusz Woźniak <mariusz.wozniak@intive.com>
2017-02-07 09:44:30 +0100

    Timeformat localized texts

--- js/mainmenu/settings/timeformat.js
+++ js/mainmenu/settings/timeformat.js
@@ -4,7 +4,12 @@
 			.subscribe(function() {
 				
 				//Localize text
-				modifyInnerHtml(page, '.ui-title', TIZEN_L10N.SETTINGS_MENU_TIMEFORMAT);
+				modifyInnerHtml(page, '.ui-title', TIZEN_L10N.SETTINGS_MENU_TIMEFORMAT);				
+				modifyElements(page, 'ul.ui-listview li input[name="timeformat-radio"]', function(inputElement) {
+					modifyInnerHtml(inputElement.parentNode,
+							'span#text',
+							storage.settings.timeformat.mapping.getLocalizedTextForValue(inputElement.value));
+				});
 				
 				addGenericHandlerForSettingPageWithRadioButtons(page,
 						storage.settings.timeformat,
