3efd16cf5aa9cbc342644aa0646094759ef9ba2d
Maciej Malak <maciej.malak@intive.com>
2017-02-09 15:39:00 +0100

    TWCGS3-29: Information about js/data/buildInfo json

--- code/js/data/buildInfo.js
+++ code/js/data/buildInfo.js
@@ -1,4 +1,7 @@
+// This file is overriden by Jenkins with real values after each Jenkins build.
+// Please review ~/CI/buildScript.sh to review details.
+// Build from Tizen studio will have dev dev values.
 const buildInfo = {
-		jenkinsBuildNumber: 0,
-		commitHash: "0000000",
+		jenkinsBuildNumber: "dev",
+		commitHash: "dev",
 	};
