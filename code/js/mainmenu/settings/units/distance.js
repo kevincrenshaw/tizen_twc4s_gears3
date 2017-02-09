32ee718ef94d4ed6a0ab89ae2a6f5fa2aadd4f08
Maciej Malak <maciej.malak@intive.com>
2017-02-09 15:33:42 +0100

    TWCGS3-29: Adding missing Rx listener for page event

--- code/js/mainmenu/settings/units/distance.js
+++ code/js/mainmenu/settings/units/distance.js
@@ -1,9 +1,12 @@
 ( function () {
 	modifyElement(document, '#settingsUnitsDistancePage.ui-page', function(page) {
-		modifyInnerHtml(page, '.ui-title', TIZEN_L10N.SETTINGS_MENU_UNITS_DISTANCE);
 		
-		addGenericHandlerForSettingPageWithRadioButtons(page,
-				storage.settings.units.distance,
-				function(el) { return el.name === 'distance-radio'; });
+		Rx.Observable.fromEvent(page, 'pagebeforeshow').subscribe(function() {
+			modifyInnerHtml(page, '.ui-title', TIZEN_L10N.SETTINGS_MENU_UNITS_DISTANCE);
+			
+			addGenericHandlerForSettingPageWithRadioButtons(page,
+					storage.settings.units.distance,
+					function(el) { return el.name === 'distance-radio'; });
+		});
 	});
 } () );
