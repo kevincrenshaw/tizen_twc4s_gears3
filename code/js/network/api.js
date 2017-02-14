c8e619895fea70facc9dd855cf6363ec23fbcdd3
Maciej Malak <maciej.malak@intive.com>
2017-02-14 11:22:03 +0100

    TWCGS3-37: Fetching resources from many URLS provided as any iterable object

--- code/js/network/api.js
+++ code/js/network/api.js
@@ -1,32 +1,41 @@
 /* jshint esversion: 6 */
-( function () {
-	
-	const getResourcesByURL = function(url, subscriber) {
-		
-		var requestedStream = Rx.Observable.just(url);
-		
-		var response = requestedStream
-			.flatMap(function(requestedUrl) {
-				return Rx.Observable.fromPromise($.get(requestedUrl));
-			});
-		
-		response.subscribe(
-				function(item) {
-					alert("item" + item.body);
-					console.log(item); 
-				},
-				function(err) {
-					alert("error"  + err);
-					console.log(err); 
-				},
-				function() {
-					alert("onCompleted");
-					console.log("complete"); 
-				}
-		);
+(function() {
+
+	// TODO be able to provide subscriber as a param not hardcoded in method
+	const getResourcesByURL = function(urls, subscriber) {
+
+		var requestedStream = Rx.Observable.from(urls);
+
+		var response = requestedStream.flatMap(function(requestedUrl) {
+			return Rx.Observable.fromPromise($.get(requestedUrl));
+		});
+
+		// TODO implement timeout mechanism
+
+		response.subscribe(function(item) {
+			// alert("item" + item.body);
+			console.log("API call success: " + item.body);
+		}, function(err) {
+			// alert("error " + err);
+			console.log("API call error: " + err);
+		}, function() {
+			// alert("onAPICallCompleted");
+			console.log("API call completed");
+		});
 	};
-	
-	console.log("API call is here");
-	getResourcesByURL('https://jsonplaceholder.typicode.com/posts/1', "");
-	
-} () );
+
+	var urls = [
+			'https://jsonplaceholder.typicode.com/posts/1',
+			'https://jsonplaceholder.typicode.com/posts/2',
+			'https://jsonplaceholder.typicode.com/posts/3',
+			'https://splashbase.s3.amazonaws.com/unsplash/regular/tumblr_mnh0n9pHJW1st5lhmo1_1280.jpg' ];
+
+	var badUrls = [
+			'https://jsonplaceholder.typicode.com/posts/1',
+			'https://jsonplaceholder.typicode.com/posts/2',
+			'https://jsonplaceholder.typicode.com/posts/3',
+			'https://splashbase.s3.amazonaws.com/unsplash/regular/tumblr_mnh0n9pHJW1st5lhmo1_1280.jpg',
+			'https://completny.bad.url.com/post/1' ];
+
+	getResourcesByURL(urls, "");
+}());
