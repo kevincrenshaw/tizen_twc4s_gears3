ef81bb3628b93eaff7ebffcaf111d9808b10e514
Mykhailo Zhyliakov <mykhailo.zhyliakov@intive.com>
2017-02-17 11:05:40 +0100

    TWCGS3-133: Added marquee styling for settings menu enties;

--- code/js/mainmenu/settings.js
+++ code/js/mainmenu/settings.js
@@ -4,8 +4,8 @@
 			.subscribe(function() {
 				//Localize
 				modifyInnerHtml(page, '.ui-title', TIZEN_L10N.MAIN_MENU_SETTINGS);
-				modifyInnerHtml(page, '.ui-listview a#units span#text', TIZEN_L10N.SETTINGS_MENU_UNITS);
-				modifyInnerHtml(page, '.ui-listview a#information span#text', TIZEN_L10N.SETTINGS_MENU_INFORMATION);
+				modifyInnerHtml(page, '.ui-listview a#units div#text', TIZEN_L10N.SETTINGS_MENU_UNITS);
+				modifyInnerHtml(page, '.ui-listview a#information div#text', TIZEN_L10N.SETTINGS_MENU_INFORMATION);
 			});
 	});
 } () );
--- code/js/mainmenu/settings/units.js
+++ code/js/mainmenu/settings/units.js
@@ -3,20 +3,21 @@
 		Rx.Observable.fromEvent(page, 'pagebeforeshow')
 			.subscribe(function() {
 				
-				//Localize text
+				//Localize text for titles
 				modifyInnerHtml(page, '.ui-title', TIZEN_L10N.SETTINGS_MENU_UNITS);
-				modifyInnerHtml(page, '.ui-listview a#time span#text', TIZEN_L10N.SETTINGS_MENU_UNITS_TIME);
-				modifyInnerHtml(page, '.ui-listview a#distance span#text', TIZEN_L10N.SETTINGS_MENU_UNITS_DISTANCE);
-				modifyInnerHtml(page, '.ui-listview a#mapzoom span#text', TIZEN_L10N.SETTINGS_MENU_UNITS_MAPZOOM);
-				modifyInnerHtml(page, '.ui-listview a#temperature span#text', TIZEN_L10N.SETTINGS_MENU_UNITS_TEMPERATURE);
+				modifyInnerHtml(page, '.ui-listview a#time div#text', TIZEN_L10N.SETTINGS_MENU_UNITS_TIME);
+				modifyInnerHtml(page, '.ui-listview a#distance div#text', TIZEN_L10N.SETTINGS_MENU_UNITS_DISTANCE);
+				modifyInnerHtml(page, '.ui-listview a#mapzoom div#text', TIZEN_L10N.SETTINGS_MENU_UNITS_MAPZOOM); 
+				modifyInnerHtml(page, '.ui-listview a#temperature div#text', TIZEN_L10N.SETTINGS_MENU_UNITS_TEMPERATURE);
 				
+				//Update text for subtitles 
 				const distanceUnit = storage.settings.units.distance.getMapped();
-				modifyInnerHtml(page, '.ui-listview a#distance span#value', distanceUnit);
+				modifyInnerHtml(page, '.ui-listview a#distance div#value', distanceUnit);
 		
-				modifyInnerHtml(page, '.ui-listview a#time span#value',
+				modifyInnerHtml(page, '.ui-listview a#time div#value',
 					storage.settings.units.time.getMapped());
 				
-				modifyInnerHtml(page, '.ui-listview a#mapzoom span#value',
+				modifyInnerHtml(page, '.ui-listview a#mapzoom div#value',
 					storage.settings.units.mapzoom.getMapped());
 				
 				//Just to print in log current value
