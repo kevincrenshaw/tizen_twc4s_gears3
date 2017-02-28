b4d2dc3e41a6b23cdbe35c1ebfe4d45b6b5ccd43
Mariusz Woźniak <mariusz.wozniak@intive.com>
2017-02-28 11:37:19 +0100

    TWCGS3-188: weather page as module

--- code/js/app.js
+++ code/js/app.js
@@ -14,6 +14,7 @@ const modules = [
 	'pages/partnerapp',
 	'pages/information',
 	'pages/radar',
+	'pages/weather',
 ];
 
 define(modules, function(require, circleHelper) {
--- code/js/pages/weather.js
+++ code/js/pages/weather.js
@@ -1,26 +1,30 @@
-( function () {
-	
+define(['utils/network'], function(network) {
 	const url = ['http://api.wunderground.com/api/abf91b89f554facf/conditions/q/CA/San_Francisco.json'];
 	const waitForMillis = 5000; 
 	var subscription = null;
 	
-	var deleteLastSession = function() {
-		storage.weatherSession.removeLastSession();
-		modifyInnerHtml(document, 'span#status', '');
+	const createLastSessionDeleter = function(root) {
+		const deleteLastSession = function() {
+			storage.weatherSession.removeLastSession();
+			modifyInnerHtml(root, 'span#status', '');
+		};
+		
+		return deleteLastSession;
 	};
-
+	
 	const radar = {
-		onPageHide : function(e) {
-			document.getElementById('delete-button').removeEventListener("click", deleteLastSession);
+		onPageHide : function(page) {
+			page.querySelector('#delete-button').removeEventListener("click", createLastSessionDeleter(page));
 			if (subscription) {
 				subscription.dispose();
-                subscription = null;
-            }
+	            subscription = null;
+	        }
 		},
 		
-		onPageShow : function(e) {
-			modifyInnerHtml(document, 'span#fetch-indicator', 'waiting for response');
-			document.getElementById('delete-button').addEventListener("click", deleteLastSession);
+		onPageShow : function(page) {
+			modifyInnerHtml(page, 'span#fetch-indicator', 'waiting for response');
+			
+			page.querySelector('#delete-button').addEventListener("click", createLastSessionDeleter(page));
 				
 			//try to load last saved sesstion and show it on UI
 			var lastSavedSession = storage.weatherSession.getSession();
@@ -31,13 +35,13 @@
 			subscription = Rx.Observable.interval(waitForMillis).subscribe(
 				function(x) {
 					//perform request
-					getResourcesByURL(url, { timeout: 0, delay: 0}).subscribe(
+					network.getResourcesByURL(url, { timeout: 0, delay: 0}).subscribe(
 						function(response) {
 							if(subscription) {
 								console.log('response fetched and saved');
-								modifyInnerHtml(document, 'span#status', '');
+								modifyInnerHtml(page, 'span#status', '');
 								const result = JSON.stringify(response);
-								modifyInnerHtml(document, 'span#status', result);
+								modifyInnerHtml(page, 'span#status', result);
 								storage.weatherSession.addSession(result);
 							} else {
 								console.log('response fetched but no listener found - exit');
@@ -47,13 +51,13 @@
 							console.error("error: " + error.status);
 							if(subscription) {
 								const result = JSON.stringify(error.status);
-								modifyInnerHtml(document, 'span#status', result);
+								modifyInnerHtml(page, 'span#status', result);
 							}
 						},
 						function() {
 							if(subscription) {
 								var timestamp = new Date();
-								modifyInnerHtml(document, 'span#fetch-indicator', 'updated at: ' + timestamp.getHours() + ':' + timestamp.getMinutes() + ':' + timestamp.getSeconds());											
+								modifyInnerHtml(page, 'span#fetch-indicator', 'updated at: ' + timestamp.getHours() + ':' + timestamp.getMinutes() + ':' + timestamp.getSeconds());											
 							}
 						});
 				},
@@ -66,10 +70,16 @@
 			console.log('subscription: ' + JSON.stringify(subscription));
 		}
 	};
-
-	modifyElement(document, '#weatherPage.ui-page', function(page) {
-		Rx.Observable.fromEvent(page, 'pagebeforeshow').subscribe(radar.onPageShow);
-		Rx.Observable.fromEvent(page, 'pagebeforehide').subscribe(radar.onPageHide);
-	});
-} () );
-
+	
+	return {
+		pagebeforeshow: function(ev) {
+			const page = ev.target;
+			radar.onPageShow(page);
+		},
+		
+		pagebeforehide: function(ev) {
+			const page = ev.target;
+			radar.onPageHide(page);
+		},
+	};
+});
