c643914ce16539f66022246c1943ef1f7eeb3ab0
Mariusz Woźniak <mariusz.wozniak@intive.com>
2017-03-01 10:34:59 +0100

    TWCGS3-219: added junit reporter

--- UT/Gruntfile.js
+++ UT/Gruntfile.js
@@ -1,11 +1,18 @@
 module.exports = function(grunt) {
 	grunt.loadNpmTasks('grunt-contrib-qunit');
+	grunt.loadNpmTasks('grunt-qunit-junit');
 
 	grunt.initConfig({
 		qunit: {
 			src: ['index.html'],
 		},
+
+		qunit_junit: {
+			options: {
+				dest: '.'
+			},
+		},
 	});
 
-	grunt.registerTask('default', ['qunit']);
+	grunt.registerTask('default', ['qunit_junit', 'qunit']);
 };
