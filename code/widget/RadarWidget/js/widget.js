30c5bacf38e4787bf50c7a10065f2de742a9b5fd
Mykhailo Zhyliakov <mykhailo.zhyliakov@intive.com>
2017-03-14 23:43:55 +0100

    TWCGS3-48: added radar widget; added click event for opening main application;

---
+++ code/widget/RadarWidget/js/widget.js
@@ -0,0 +1,33 @@
+window.onload = function() {
+	var startButton = null;
+	
+	//run for first time when widget is added to a widget board
+	handleVisibilityChange();
+	
+	function handleVisibilityChange() {
+		console.log('visibility: ' + document.visibilityState);
+		if(document.visibilityState === 'visible') {
+			startButton = document.getElementById('main-screen');
+			startButton.addEventListener('click', launchApp);
+		} else if(startButton) {
+			startButton.removeEventListener('click', launchApp);
+			startButton = null;
+		}
+	}
+	
+	function launchApp() {
+        var app = window.tizen.application.getCurrentApplication();
+        var appId = app.appInfo.id.substring(0, (app.appInfo.id.lastIndexOf('.')) );
+        var appControl = new window.tizen.ApplicationControl('', null, null, null, null, null);
+        window.tizen.application.launchAppControl(appControl, appId,
+            function() {
+                console.log("application has been launched successfully");
+            },
+            function(e) {
+                console.error("application launch has been failed. reason: " + e.message);
+            },
+            null);
+      }
+	
+	document.addEventListener('visibilitychange', handleVisibilityChange);
+};
