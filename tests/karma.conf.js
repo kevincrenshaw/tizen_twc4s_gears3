b0192904afad9daa5577bea7b0c5e6623f12bffe
marek.supruniuk <marek.supruniuk@intive.com>
2017-05-09 09:14:51 +0200

    TWCGS3-443: added eslint

--- tests/karma.conf.js
+++ tests/karma.conf.js
@@ -43,7 +43,16 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
-        './code/js/**/*.js': ['coverage']
+        './tests/specs/js/**/*.js': ['eslint'],
+        './code/js/**/*.js': ['eslint', 'coverage']
+    },
+
+
+    eslint: {
+      stopOnError: true,
+      engine: {
+        configFile: './.eslintrc'
+      }
     },
 
 
